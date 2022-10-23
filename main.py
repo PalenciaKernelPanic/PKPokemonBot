f7e930f5036ddf0019f18dd6b21aebbffe3abd51
jorgegomez <jorgegomez@bmat.com>
2022-10-23 11:53:07 +0200

    better code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import telegram
+from typing import Any, Tuple, Union
+
 from telegram import Update, ParseMode
 from telegram.ext import Updater, CallbackContext, CommandHandler, Defaults
 from telegram.bot import Bot, BotCommand
@@ -6,7 +7,7 @@ import logging
 import importlib
 
 import config
-
+from _version import __version__
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -16,89 +17,73 @@ def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 
 
+def check_args(context: CallbackContext) -> Tuple[bool, Union[bool, str], Union[bool, str], Union[bool, Any]]:
+    ok = class_name = msg = t = False
+    if len(context.args) == 0:
+        msg = 'Tienes que especificar un tipo'
+    elif not is_ascii(context.args[0]):
+        msg = 'Escribe el tipo sin tildes'
+    else:
+        ok = True
+        class_name = context.args[0].capitalize()
+        MiTipo = getattr(importlib.import_module('tablatipos.tipos'), class_name)
+        t = MiTipo()
+    return ok, class_name, msg, t
+
+
 def tabla_tipos(update: Update, context: CallbackContext):
     update.message.reply_photo(photo='https://static.wikia.nocookie.net/pokemonreloaded/images/3/39/Efectividades.png/revision/latest/scale-to-width-down/680?cb=20141226072815&path-prefix=es')
 
 
 def eficaz_contra(update: Update, context: CallbackContext):
-    if len(context.args) == 0:
-        msg = 'Tienes que especificar un tipo'
-    elif not is_ascii(context.args[0]):
-        msg = 'Escribe el tipo sin tildes'
-    else:
-        class_name = context.args[0].capitalize()
-        MiTipo = getattr(importlib.import_module('tablatipos.tipos'), class_name)
-        t = MiTipo()
+    ok, class_name, msg, t = check_args(context=context)
+    if ok:
         msg = f'El tipo *{class_name}* es __eficaz contra__: _{t.get_eficaz_contra()}_'
     update.message.reply_text(text=msg)
 
 
 def poco_eficaz_contra(update: Update, context: CallbackContext):
-    if len(context.args) == 0:
-        msg = 'Tienes que especificar un tipo'
-    elif not is_ascii(context.args[0]):
-        msg = 'Escribe el tipo sin tildes'
-    else:
-        class_name = context.args[0].capitalize()
-        MiTipo = getattr(importlib.import_module('tablatipos.tipos'), class_name)
-        t = MiTipo()
+    ok, class_name, msg, t = check_args(context=context)
+    if ok:
         msg = f'El tipo *{class_name}* es __poco eficaz contra__: _{t.get_poco_eficaz_contra()}_'
     update.message.reply_text(text=msg)
 
 
 def resistente_ante(update: Update, context: CallbackContext):
-    if len(context.args) == 0:
-        msg = 'Tienes que especificar un tipo'
-    elif not is_ascii(context.args[0]):
-        msg = 'Escribe el tipo sin tildes'
-    else:
-        class_name = context.args[0].capitalize()
-        MiTipo = getattr(importlib.import_module('tablatipos.tipos'), class_name)
-        t = MiTipo()
+    ok, class_name, msg, t = check_args(context=context)
+    if ok:
         msg = f'El tipo *{class_name}* es __resistente ante__: _{t.get_resistente_ante()}_'
     update.message.reply_text(text=msg)
 
 
 def debil_ante(update: Update, context: CallbackContext):
-    if len(context.args) == 0:
-        msg = 'Tienes que especificar un tipo'
-    elif not is_ascii(context.args[0]):
-        msg = 'Escribe el tipo sin tildes'
-    else:
-        class_name = context.args[0].capitalize()
-        MiTipo = getattr(importlib.import_module('tablatipos.tipos'), class_name)
-        t = MiTipo()
+    ok, class_name, msg, t = check_args(context=context)
+    if ok:
         msg = f'El tipo *{class_name}* es __debil ante__: _{t.get_debil_ante()}_'
     update.message.reply_text(text=msg)
 
 
 def stats(update: Update, context: CallbackContext):
-    if len(context.args) == 0:
-        msg = 'Tienes que especificar un tipo'
-    elif not is_ascii(context.args[0]):
-        msg = 'Escribe el tipo sin tildes'
-    else:
-        class_name = context.args[0].capitalize()
-        MiTipo = getattr(importlib.import_module('tablatipos.tipos'), class_name)
-        t = MiTipo()
+    ok, class_name, msg, t = check_args(context=context)
+    if ok:
         msg = f'''El tipo *{class_name}* es:
-        \- eficaz contra: _{t.get_eficaz_contra()}_
-        \- poco eficaz contra: _{t.get_poco_eficaz_contra()}_
-        \- resistente ante: _{t.get_resistente_ante()}_
-        \- debil ante: _{t.get_debil_ante()}_'''
+\- eficaz contra: _{t.get_eficaz_contra()}_
+\- poco eficaz contra: _{t.get_poco_eficaz_contra()}_
+\- resistente ante: _{t.get_resistente_ante()}_
+\- debil ante: _{t.get_debil_ante()}_'''
     update.message.reply_text(text=msg)
 
 
 def help(update: Update, context: CallbackContext):
-    msg = """
+    msg = f"""v{__version__}
 /help: Muestra un mensaje de ayuda
 /tabla\_tipos: Muestra la tabla de tipos
-/stats _\<tipo\>_: Muestra eficacia, resistencia y debilidad contra otros tipos
-/eficaz\_contra _\<tipo\>_: Tipos contra los que es eficaz
-/poco\_eficaz\_contra _\<tipo\>_: Tipos contra los que es poco eficaz
-/resistente\_ante _\<tipo\>_: Tipos antes los que es resistente
-/debil\_ante _\<tipo\>_: Tipos ante los que es débil
-    """
+/stats _<tipo>_: Muestra eficacia, resistencia y debilidad contra otros tipos
+/eficaz\_contra _<tipo>_: Tipos contra los que es eficaz
+/poco\_eficaz\_contra _<tipo>_: Tipos contra los que es poco eficaz
+/resistente\_ante _<tipo>_: Tipos antes los que es resistente
+/debil\_ante _<tipo>_: Tipos ante los que es débil
+    """.replace('.', '\\.').replace('<', '\\<').replace('>', '\\>')
     update.message.reply_text(text=msg)
 
 
